dd4d97cf071956ea12ea4fcfdc5b9b9a53f7a7d0
Angel Barrera <angelbarrerasanchez@gmail.com>
2023-06-17 11:23:46 +0200

    Adapt interface to allow returning the additional labels

--- internal/app/prometheus-multi-tenant-proxy/auth.go
+++ internal/app/prometheus-multi-tenant-proxy/auth.go
@@ -10,7 +10,7 @@ type key int
 // Auth implements an authentication middleware
 type Auth interface {
 	// IsAuthorized authenticates a request and returns the list of namespaces the user has access to
-	IsAuthorized(r *http.Request) (bool, []string)
+	IsAuthorized(r *http.Request) (bool, []string, map[string]string)
 	// WriteUnauthorisedResponse writes an HTTP response in case the user is forbidden
 	WriteUnauthorisedResponse(w http.ResponseWriter)
 	// Load loads or reloads the configuration
@@ -20,12 +20,13 @@ type Auth interface {
 // AuthHandler returns au authentication middleware handler
 func AuthHandler(auth Auth, handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		authorized, namespaces := auth.IsAuthorized(r)
+		authorized, namespaces, labels := auth.IsAuthorized(r)
 		if !authorized {
 			auth.WriteUnauthorisedResponse(w)
 			return
 		}
 		ctx := context.WithValue(r.Context(), Namespaces, namespaces)
+		ctx = context.WithValue(ctx, Labels, labels)
 		handler(w, r.WithContext(ctx))
 	}
 }
